cd087e3f985cdb9427ea83b6cb716e9cd786f9c8
zachaa2 <aaron.zachariah@gmail.com>
2026-05-08 21:05:44 -0400

    footer, styles and icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,10 @@
-const go = new Go()
-const result = await WebAssembly.instantiateStreaming(fetch("/main.wasm"), go.importObject)
-go.run(result.instance)
-document.querySelector('#app').textContent = goHello()
+const go = new Go();
+
+const result = await WebAssembly.instantiateStreaming(
+  fetch("/main.wasm"),
+  go.importObject,
+);
+
+go.run(result.instance);
+
+document.querySelector("#app").textContent = goHello();
